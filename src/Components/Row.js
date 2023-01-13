3fa444d9744b87d6a545ca5de0e6dc853b12bfd2
Abdullah <shabdullah685@gmail.com>
2023-01-13 14:52:43 +0530

    fetching movie data

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../axios';
+import './Row.css';
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl }) {
-    const [movie, setMovie] = useState([])
+    const [movies, setMovies] = useState([])
 
     // A snippet of code which runs based on a specific conditions/variables
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request)
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, [])
+    }, [fetchUrl])
 
+    console.log(movies)
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"
+                        src={`${base_url}${movie.poster_path}`}
+                        alt={movie.name} />
+                ))}
+            </div>
         </div>
     )
 }
