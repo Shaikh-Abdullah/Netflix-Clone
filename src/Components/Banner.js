36869875b3791f944cdda8ca54fd5c09451543e8
Abdullah <shabdullah685@gmail.com>
2023-01-14 17:15:19 +0530

    almost done

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -27,7 +27,7 @@ function Banner() {
                 backgroundSize: "cover",
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
                 )`,
-                backgroundPosition: "center center"
+                backgroundPosition: "top"
             }}
         >
             <div className="banner__contents">
