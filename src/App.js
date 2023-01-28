d5ba9220dca01af6899d55e9148715a5d25e1c26
Abdullah <shabdullah685@gmail.com>
2023-01-28 21:49:19 +0530

    Firebase SignIn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import HomeScreen from "./Components/HomeScreen";
 import LoginScreen from "./Components/LoginScreen";
+import { auth } from "./firebase";
 
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+
+    })
+  }, [])
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyAsqSIdYuZAZh5dkgTWd-p0gGfqVu9YcFQ",
     authDomain: "netfilx-2-0.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "66540847061",
     appId: "1:66540847061:web:3ffddf6f55fbc12533875c"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
