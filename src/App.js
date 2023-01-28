654998c42c310b27e297bec4153a839e947fee5b
Abdullah <shabdullah685@gmail.com>
2023-01-28 19:17:00 +0530

    login and signUp screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-import Banner from './Components/Banner';
-import Nav from './Components/Nav';
-import Row from './Components/Row';
-import requests from "./requests"
+import HomeScreen from "./Components/HomeScreen";
+import LoginScreen from "./Components/LoginScreen";
+
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumantaries} />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/" exact>
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+
+      </Router>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsqSIdYuZAZh5dkgTWd-p0gGfqVu9YcFQ",
+    authDomain: "netfilx-2-0.firebaseapp.com",
+    projectId: "netfilx-2-0",
+    storageBucket: "netfilx-2-0.appspot.com",
+    messagingSenderId: "66540847061",
+    appId: "1:66540847061:web:3ffddf6f55fbc12533875c"
+};
