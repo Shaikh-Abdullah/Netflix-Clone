b60448cbdd58500264a17b30746d1d440d89dd3a
Abdullah <shabdullah685@gmail.com>
2023-01-28 20:10:22 +0530

    Starting Firebase Auth

--- src/Components/SignupScreen.js
+++ src/Components/SignupScreen.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './SignupScreen.css';
 
 function SignupScreen() {
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const signIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>SignupScreen</div>
+        <div className='signupScreen'>
+            <form>
+                <h1>Sign In</h1>
+                <input type="mail" placeholder='Email' />
+                <input type="password" placeholder='password' />
+                <button type='submit' onClick={signIn}>Sign In</button>
+
+                <h4>
+                    <span className='signupScreen__gray'>New to Netflix? </span>
+                    <span className='signupScreen__link' onClick={register}>Sign Up now</span>
+                </h4>
+            </form>
+        </div>
     )
 }
 
