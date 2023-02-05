0cce0c90de3a781447e3b021100555377c3915e6
Abdullah <shabdullah685@gmail.com>
2023-02-05 15:19:01 +0530

    plans screen done

--- src/Components/PlansScreen.js
+++ src/Components/PlansScreen.js
@@ -1,26 +1,34 @@
-import React from 'react'
-import './PlansScreen.css'
+import React from 'react';
+import './PlansScreen.css';
+// import { loadStripe } from '@stripe/stripe-js'
 
 function PlansScreen() {
 
+
     return (
-        <div className='plansScreen__plan'>
-            <div className="plansScreen__info">
-                <h5>Premium</h5>
-                <h6>4K + HDR</h6>
+        <>
+            <div className='plansScreen__plan'>
+                <div className="plansScreen__info">
+                    <h5>Premium</h5>
+                    <h6>4K + HDR</h6>
+                </div>
+                <button>Subscribe</button>
             </div>
-            <button>Subscribe</button>
-            <div className="plansScreen__info">
-                <h5>Standard</h5>
-                <h6>1080p</h6>
+            <div className='plansScreen__plan'>
+                <div className="plansScreen__info">
+                    <h5>Standard</h5>
+                    <h6>1080p</h6>
+                </div>
+                <button>Subscribe</button>
             </div>
-            <button>Subscribe</button>
-            <div className="plansScreen__info">
-                <h5>Basic</h5>
-                <h6>720p</h6>
+            <div className='plansScreen__plan'>
+                <div className="plansScreen__info">
+                    <h5>Basic</h5>
+                    <h6>720p</h6>
+                </div>
+                <button>Subscribe</button>
             </div>
-            <button>Subscribe</button>
-        </div>
+        </>
     )
 }
 
